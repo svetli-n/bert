c745f99d357a2b6e3676928b1ba4e03d7abcb9cb
Svetlin <svetlin.stefanov@inoviagroup.se>
2019-01-28 10:39:38 +0100

    Batch tf serving requests works.

--- run_classifier.py
+++ run_classifier.py
@@ -824,7 +824,8 @@ def make_request():
     max_seq_length = 128
     vocab_file = '/Users/svetlin/workspace/q-and-a/bert-data/cased_L-12_H-768_A-12/vocab.txt'
     data_dir = '.'
-    examples_file = '/Users/svetlin/workspace/q-and-a/glue_data/QNLI/dev.tsv.short'
+    dev_file = '/Users/svetlin/workspace/q-and-a/glue_data/QNLI/dev.tsv.short'
+    train_file = '/Users/svetlin/workspace/q-and-a/glue_data/QNLI/train.tsv.short'
 
     channel = grpc.insecure_channel("localhost:8500")
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
@@ -834,27 +835,38 @@ def make_request():
     processor = QnliProcessor(data_dir)
     label_list = processor.get_labels()
 
-    with open(examples_file) as fr:
-       lines = fr.readlines()
+    with open(dev_file) as dev_fh:
+       dev_lines = dev_fh.readlines()
 
-    for line in lines[1:]:
-        example = line[:-1].split('\t')
+    with open(train_file) as train_fh:
+        train_lines = train_fh.readlines()
 
-        inputExample = processor._create_examples([None, example], 'test')[0]
-        tf_example = from_record_to_tf_example(3, inputExample, label_list, max_seq_length, tokenizer)
-        model_input = tf_example.SerializeToString()
+    for dev_line, train_line in zip(dev_lines[1:], train_lines[1:]):
+        train_example = train_line[:-1].split('\t')
+        dev_example = dev_line[:-1].split('\t')
+
+        input_li = [dev_example for _ in range(20)]
+        inputExample = processor._create_examples(input_li, 'test')
+        model_input = []
+
+        for example in inputExample:
+            tf_example = from_record_to_tf_example(3, example, label_list, max_seq_length, tokenizer)
+            model_input.append(tf_example.SerializeToString())
 
         # Send request
         # See prediction_service.proto for gRPC request/response details.
         model_request = predict_pb2.PredictRequest()
         model_request.model_spec.name = 'my_model'
-        # model_request.model_spec.signature_name = 'serving_default'
-        dims = [tensor_shape_pb2.TensorShapeProto.Dim(size=1)]
-        tensor_shape_proto = tensor_shape_pb2.TensorShapeProto(dim=dims)
-        tensor_proto = tensor_pb2.TensorProto(
-            dtype=types_pb2.DT_STRING,
-            tensor_shape=tensor_shape_proto,
-            string_val=[model_input])
+        model_request.model_spec.signature_name = 'serving_default'
+
+        # dims = [tensor_shape_pb2.TensorShapeProto.Dim(size=2)]
+        # tensor_shape_proto = tensor_shape_pb2.TensorShapeProto(dim=dims)
+        # tensor_proto = tensor_pb2.TensorProto(
+        #     dtype=types_pb2.DT_STRING,
+        #     tensor_shape=tensor_shape_proto,
+        #     string_val=[model_input])
+
+        tensor_proto = tf.contrib.util.make_tensor_proto(model_input, shape=[len(inputExample)])
 
         model_request.inputs['examples'].CopyFrom(tensor_proto)
         result = stub.Predict(model_request, 10.0)  # 10 secs timeout
@@ -862,7 +874,7 @@ def make_request():
         # pretty_result = "Predicted Label: " + label_list[result[0].argmax(axis=0)]
         # tf.logging.info("Predicted Label: %s", label_list[result[0].argmax(axis=0)])
         # tf.logging.info('Result: %s', pretty_result)
-        print(result[0])
+        print(result)
         # print(pretty_result)
 
 
